408d446b56175f20e1a61b5a940b1ce74cd7d86b
Binh Nguyen <sometimesocrazy@gmail.com>
2024-03-24 10:24:57 +0700

    - submit

---
+++ src/environments/environment.prd.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  firebaseConfig:{
+    apiKey: "AIzaSyAwxXYVj-fH6lw7fJGtthBHs0W3d_CdB8o",
+    authDomain: "omelet-f0b89.firebaseapp.com",
+    databaseURL: "https://omelet-f0b89-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "omelet-f0b89",
+    storageBucket: "omelet-f0b89.appspot.com",
+    messagingSenderId: "401458936175",
+    appId: "1:401458936175:web:21ae4536e13b8b038fd09a"
+  },
+  production: false,
+  api_url: "https://identify-api.omelet.tech/",
+  TAILWIND_MODE: "build",
+  upload_end_point: "http://127.0.0.1:8080/editor/upload/"
+
+};
